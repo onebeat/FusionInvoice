f780079567cc96f0824fcc9cd7ffa6faed0fa516
Jesse Terry <jessedterry@gmail.com>
2013-10-19 00:51:28 -0700

    Added post route for settings controller

--- app/routes.php
+++ app/routes.php
@@ -136,6 +136,7 @@ Route::group(array('before' => 'auth'), function()
 	|--------------------------------------------------------------------------
 	*/
 	Route::get('settings', array('uses' => 'SettingController@index', 'as' => 'settings.index'));
+	Route::post('settings', array('uses' => 'SettingController@update', 'as' => 'settings.update'));
 
 	/*
 	|--------------------------------------------------------------------------
