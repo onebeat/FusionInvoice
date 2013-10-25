dbb708780d0cccd7ad23e93a91619b4b46fcaf58
Jesse Terry <jessedterry@gmail.com>
2013-10-24 20:39:22 -0700

    Scaling the routes back a bit for now

--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,11 @@
 <?php
 
+App::before(function($request)
+{
+	$settings = App::make('FI\Storage\Interfaces\SettingRepositoryInterface');
+	$settings->setAll();
+});
+
 Route::get('login', array('uses' => 'SessionController@login', 'as' => 'session.login'));
 Route::post('login', array('uses' => 'SessionController@attempt', 'as' => 'session.attempt'));
 Route::get('logout', array('uses' => 'SessionController@logout', 'as' => 'session.logout'));
@@ -32,53 +38,6 @@ Route::group(array('before' => 'auth'), function()
 
 	/*
 	|--------------------------------------------------------------------------
-	| Routes: Invoices
-	|--------------------------------------------------------------------------
-	*/
-	Route::get('invoices', array('uses' => 'InvoiceController@index', 'as' => 'invoices.index'));
-	Route::get('invoices/recurring', array('uses' => 'RecurringInvoiceController@index', 'as' => 'invoices.recurring.index'));
-	Route::get('invoices/modal/create', array('uses' => 'InvoiceController@modalCreate', 'as' => 'invoices.ajax.modalCreate'));
-	Route::get('invoices/modal/copy', array('uses' => 'InvoiceController@modalCopy', 'as' => 'invoices.ajax.modalCopy'));
-	Route::get('invoices/modal/create/client', array('uses' => 'InvoiceController@modalCreateByClient', 'as' => 'invoices.ajax.modalCreateByClient'));
-
-	/*
-	|--------------------------------------------------------------------------
-	| Routes: Quotes
-	|--------------------------------------------------------------------------
-	*/
-	Route::get('quotes', array('uses' => 'QuoteController@index', 'as' => 'quotes.index'));
-	Route::get('quotes/modal/create', array('uses' => 'QuoteController@modalCreate', 'as' => 'quotes.ajax.modalCreate'));
-	Route::get('quotes/modal/invoice', array('uses' => 'QuoteController@modalQuoteToInvoice', 'as' => 'quotes.ajax.modalQuoteToInvoice'));
-	Route::get('quotes/modal/copy', array('uses' => 'QuoteController@modalCopy', 'as' => 'quotes.ajax.modalCopy'));
-	Route::get('quotes/modal/create/client', array('uses' => 'QuoteController@modalCreateByClient', 'as' => 'quotes.ajax.modalCreateByClient'));
-
-	/*
-	|--------------------------------------------------------------------------
-	| Routes: Payments
-	|--------------------------------------------------------------------------
-	*/
-	Route::get('payments', array('uses' => 'PaymentController@index', 'as' => 'payments.index'));
-	Route::get('payments/create', array('uses' => 'PaymentController@create', 'as' => 'payments.create'));
-	Route::get('payments/modal/create', array('uses' => 'PaymentController@modalCreate', 'as' => 'payments.ajax.modalCreate'));
-
-	/*
-	|--------------------------------------------------------------------------
-	| Routes: Reports
-	|--------------------------------------------------------------------------
-	*/
-	Route::get('reports/invoice_aging', array('uses' => 'ReportController@invoiceAging', 'as' => 'reports.invoiceAging'));
-	Route::get('reports/sales_by_client', array('uses' => 'ReportController@salesByClient', 'as' => 'reports.salesByClient'));
-	Route::get('reports/payment_history', array('uses' => 'ReportController@paymentHistory', 'as' => 'reports.paymentHistory'));
-
-	/*
-	|--------------------------------------------------------------------------
-	| Routes: Custom Fields
-	|--------------------------------------------------------------------------
-	*/
-	Route::get('custom_fields', array('uses' => 'CustomFieldController@index', 'as' => 'customFields.index'));
-
-	/*
-	|--------------------------------------------------------------------------
 	| Routes: Email Templates
 	|--------------------------------------------------------------------------
 	*/
@@ -92,13 +51,6 @@ Route::group(array('before' => 'auth'), function()
 
 	/*
 	|--------------------------------------------------------------------------
-	| Routes: Import
-	|--------------------------------------------------------------------------
-	*/
-	Route::get('import', array('uses' => 'ImportController@index', 'as' => 'import.index'));
-
-	/*
-	|--------------------------------------------------------------------------
 	| Routes: Invoice Groups
 	|--------------------------------------------------------------------------
 	*/
@@ -109,13 +61,6 @@ Route::group(array('before' => 'auth'), function()
 
 	Route::post('invoice_groups', array('uses' => 'InvoiceGroupController@store', 'as' => 'invoiceGroups.store'));
 	Route::post('invoice_groups/{invoiceGroup}', array('uses' => 'InvoiceGroupController@update', 'as' => 'invoiceGroups.update'));
-
-	/*
-	|--------------------------------------------------------------------------
-	| Routes: Item Lookups
-	|--------------------------------------------------------------------------
-	*/
-	Route::get('item_lookups', array('uses' => 'ItemLookupController@index', 'as' => 'itemLookups.index'));
 
 	/*
 	|--------------------------------------------------------------------------
@@ -151,10 +96,4 @@ Route::group(array('before' => 'auth'), function()
 	Route::post('tax_rates', array('uses' => 'TaxRateController@store', 'as' => 'taxRates.store'));
 	Route::post('tax_rates/{taxRate}', array('uses' => 'TaxRateController@update', 'as' => 'taxRates.update'));
 
-	/*
-	|--------------------------------------------------------------------------
-	| Routes: Users
-	|--------------------------------------------------------------------------
-	*/
-	Route::get('users', array('uses' => 'UserController@index', 'as' => 'users.index'));
 });
