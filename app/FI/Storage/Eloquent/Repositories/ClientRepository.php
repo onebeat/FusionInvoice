7e73ea2d510dbfee462c3c7ff1270eee227ab1db
Jesse Terry <antithesis@localhost.localdomain>
2013-10-25 21:59:25 -0700

    Documented methods and added lookupByName method

--- app/FI/Storage/Eloquent/Repositories/ClientRepository.php
+++ app/FI/Storage/Eloquent/Repositories/ClientRepository.php
@@ -4,39 +4,85 @@ use \FI\Storage\Eloquent\Models\Client;
 
 class ClientRepository implements \FI\Storage\Interfaces\ClientRepositoryInterface {
 	
+	/**
+	 * Get a list of all clients
+	 * @return Clients
+	 */
 	public function all()
 	{
 		return Client::all();
 	}
 
+	/**
+	 * @TODO Refactor to single getPagedByStatus method
+	 */
 	public function getPagedActive($page = 1, $numPerPage = null)
 	{
 		\DB::getPaginator()->setCurrentPage($page);
 		return Client::orderBy('name')->where('active', 1)->paginate($numPerPage ?: \Config::get('defaultNumPerPage'));
 	}
 
+	/**
+	 * @TODO Refactor to single getPagedByStatus method
+	 */
 	public function getPagedInactive($page = 1, $numPerPage = null)
 	{
 		\DB::getPaginator()->setCurrentPage($page);
 		return Client::orderBy('name')->where('active', 0)->paginate($numPerPage ?: \Config::get('defaultNumPerPage'));
 	}
 
+	/**
+	 * @TODO Refactor to single getPagedByStatus method
+	 */
 	public function getPagedAll($page = 1, $numPerPage = null)
 	{
 		\DB::getPaginator()->setCurrentPage($page);
 		return Client::orderBy('name')->paginate($numPerPage ?: \Config::get('defaultNumPerPage'));
 	}
 
+	/**
+	 * Provides a json encoded array of matching client names
+	 * @param  string $name
+	 * @return json
+	 */
+	public function lookupByName($name)
+	{
+		$clients = Client::select('name')->orderBy('name')->where('name', 'like', '%' . $name . '%')->get();
+
+		$return = array();
+
+		foreach ($clients as $client)
+		{
+			$return[] = $client->name;
+		}
+
+		return json_encode($return);
+	}
+
+	/**
+	 * Retrieve a single client record
+	 * @param  int $id
+	 * @return Client
+	 */
 	public function find($id)
 	{
 		return Client::find($id);
 	}
 	
+	/**
+	 * Create a new client record
+	 * @param  array $input
+	 */
 	public function create($input)
 	{
 		Client::create($input);
 	}
 	
+	/**
+	 * Update an existing client record
+	 * @param  array $input
+	 * @param  int $id
+	 */
 	public function update($input, $id)
 	{
 		$client = Client::find($id);
@@ -44,6 +90,10 @@ class ClientRepository implements \FI\Storage\Interfaces\ClientRepositoryInterfa
 		$client->save();
 	}
 	
+	/**
+	 * Delete a client record
+	 * @param  int $id
+	 */
 	public function delete($id)
 	{
 		Client::destroy($id);
