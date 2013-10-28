7fbf8ee6e4184d72bd2a89f3c3be21677f617b77
Jesse Terry <antithesis@localhost.localdomain>
2013-10-28 15:17:15 -0700

    Added basic currency library for formatting

---
+++ app/FI/Libraries/Currency.php
@@ -0,0 +1,15 @@
+<?php namespace FI\Libraries;
+
+class Currency {
+	
+	public static function format($amount)
+	{
+		if (\Config::get('fi.currencySymbolPlacement') == 'before')
+		{
+			return \Config::get('fi.currencySymbol') . $amount;
+		}
+
+		return $amount . \Config::get('fi.currencySymbol');
+	}
+
+}
