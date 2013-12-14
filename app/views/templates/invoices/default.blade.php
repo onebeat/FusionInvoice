4e98545c97224794cfd1f44ac9dcf0d78a2043ca
jesseterry <jessedterry@gmail.com>
2013-12-13 21:55:24 -0700

    Well what the shit...

--- app/views/templates/invoices/default.blade.php
+++ app/views/templates/invoices/default.blade.php
@@ -17,253 +17,161 @@
     <script>(function(H){H.className=H.className.replace(/\bno-js\b/,'js')})(document.documentElement)</script>
 </head>
 <body>
-<!-- begin markup -->
+    <!-- begin markup -->
+
+    <div id="invoice" class="new">
 
 
+        <div class="this-is">
+            <strong>{{ trans('fi.invoice') }}</strong>
+        </div><!-- invoice headline -->
 
-<div id="invoice" class="new">
-
-
-    <div class="this-is">
-        <strong>{{ trans('fi.invoice') }}</strong>
-    </div><!-- invoice headline -->
-
-
-    <header id="header">
-        <div class="invoice-intro">
-            <h1>{{ $invoice->user->company }}</h1>
-        </div>
-
-        <dl class="invoice-meta">
-            <dt class="invoice-number">{{ trans('fi.invoice') }} #</dt>
-            <dd>{{ $invoice->number }}</dd>
-            <dt class="invoice-date">{{ trans('fi.invoice_date') }}</dt>
-            <dd>{{ $invoice->formatted_created_at }}</dd>
-            <dt class="invoice-due">{{ trans('fi.due_date') }}</dt>
-            <dd>{{ $invoice->formatted_due_at }}</dd>
-        </dl>
-    </header>
-    <!-- e: invoice header -->
-
-
-    <section id="parties">
-
-        <div class="invoice-to">
-            <h2>{{ trans('fi.invoice_to') }}:</h2>
-            <div class="vcard">
-                {{ $invoice->client->name }}
-                {{ $invoice->client->email }}
-                
-                <div class="adr">
-                    <div class="street-address">{{ $invoice->client->address_1 }}</div>
-                    <span class="locality">{{ $invoice->client->city }}, {{ $invoice->client->state }} {{ $invoice->client->zip }}</span>
-                    <span class="country-name">{{ $invoice->client->country }}</span>
-                </div>
-
-                <div class="tel">{{ $invoice->client->phone }}</div>
-            </div><!-- e: vcard -->
-        </div><!-- e invoice-to -->
-
-
-        <div class="invoice-from">
-            <h2>{{ trans('fi.invoice_from') }}:</h2>
-            <div class="vcard">
-                {{ $invoice->user->name }}
-
-                <div class="adr">
-                    <div class="street-address">{{ $invoice->user->address_1 }}</div>
-                    <span class="locality">{{ $invoice->user->city }}, {{ $invoice->user->state }} {{ $invoice->user->zip }}</span>
-                    <span class="country-name">{{ $invoice->user->country }}</span>
-                </div>
-
-                <div class="tel">{{ $invoice->user->phone }}</div>
-                {{ $invoice->user->email }}
-            </div><!-- e: vcard -->
-        </div><!-- e invoice-from -->
-
-
-        <div class="invoice-status">
-            <h3>Invoice Status</h3>
-            <strong>Invoice is <em>{{ trans('fi.' . $invoice->status_text) }}</em></strong>
-        </div><!-- e: invoice-status -->
-
-    </section><!-- e: invoice partis -->
-
-
-    <section class="invoice-financials">
-
-        <div class="invoice-items">
-            <table>
-                <thead>
-                    <tr>
-                        <th>{{ trans('fi.item') }}</th>
-                        <th>{{ trans('fi.description') }}
-                        <th>{{ trans('fi.quantity') }}</th>
-                        <th>{{ trans('fi.price') }}</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($invoice->items as $item)
-                    <tr>
-                        <td>{{ $item->name }}</th>
-                        <td>{{ $item->description }}</td>
-                        <td>{{ $item->formatted_quantity }}</td>
-                        <td>{{ $item->formatted_price }}</td>
-                    </tr>
-                    @endforeach
-                </tbody>
-
-            </table>
-        </div><!-- e: invoice items -->
-
-
-        <div class="invoice-totals">
-            <table>
-                <caption>{{ trans('fi.totals') }}:</caption>
-                <tbody>
-                    <tr>
-                        <th>{{ trans('fi.subtotal') }}:</th>
-                        <td></td>
-                        <td>{{ $invoice->amount->formatted_item_subtotal }}</td>
-                    </tr>
-
-                    @foreach ($invoice->items as $item)
-                    @if ($item->amount->tax_total <> 0)
-                    <tr>
-                        <th>{{ trans('fi.tax') }}:</th>
-                        <td>{{ $item->tax_rate->formatted_percent }} ({{ $item->tax_rate->name }})</td>
-                        <td>{{ $item->amount->formatted_tax_total }}</td>
-                    </tr>
-                    @endif
-                    @endforeach
-
-                    @foreach ($invoice->tax_rates as $invoiceTax)
-                    <tr>
-                        <th>{{ trans('fi.tax') }}:</th>
-                        <td>{{ $invoiceTax->tax_rate->formatted_percent }} ({{ $invoiceTax->tax_rate->name }})</td>
-                        <td>{{ $invoiceTax->formatted_tax_total }}</td>
-                    </tr>
-                    @endforeach
-
-                    <tr>
-                        <th>{{ trans('fi.total') }}:</th>
-                        <td></td>
-                        <td>{{ $invoice->amount->formatted_total }}</td>
-                    </tr>
-                    <tr>
-                        <th>{{ trans('fi.paid') }}
-                        <td></td>
-                        <td>{{ $invoice->amount->formatted_paid }}</td>
-                    </tr>
-                    <tr>
-                        <th>{{ trans('fi.balance') }}:</th>
-                        <td></td>
-                        <td>{{ $invoice->amount->formatted_balance }}</td>
-                    </tr>
-                </tbody>
-            </table>
-
-        </div><!-- e: invoice totals -->
-
-
-        <div class="invoice-notes">
-            <h6>Notes &amp; Information:</h6>
-            <p>{{ $invoice->formatted_terms }}</p>
-        </div><!-- e: invoice-notes -->
-
-    </section><!-- e: invoice financials -->
-
-
-    <footer id="footer">
-        <p>
-            Custom invoice footer content
-        </p>
-    </footer>
-
-
-</div><!-- e: invoice -->
-
-
-</body>
-</html>
-<?php exit; ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <link rel="stylesheet" href="{{ asset('assets/invoices/invoice.css') }} " type="text/css" charset="utf-8" />
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script> 
-    <script type="text/javascript" src="{{ asset('assets/invoices/js/jquery.tablesorter.js') }}"></script> 
-    <script type="text/javascript">
-      $(document).ready(function() 
-      { 
-          $("#table").tablesorter({ 
-              widgets: ['zebra'] 
-          }); 
-      }); 
-    </script>
-
-    <title>{{ trans('fi.invoice') }} #{{ $invoice->number }}</title>
-
-</head>
-
-<body>
-
-    <div id="page">
-
-        <div class="status {{ $invoice->status_text }}"></div>
-        
-        <p class="recipient-address">
-            <strong>{{ $invoice->client->name }}</strong><br />
-            {{ $invoice->client->address_1 }}<br />
-            {{ $invoice->client->city }}, {{ $invoice->client->state }} {{ $invoice->client->zip }}
-        </p>
-
-        <h1>{{ trans('fi.invoice') }} #{{ $invoice->number }}</h1>
-        <p class="terms"><strong>{{ $invoice->formatted_created_at }}</strong><br/>
-            {{ trans('fi.due_date') }} {{ $invoice->formatted_due_at }}</p>
-
-            <img src="{{ asset('uploads/' . Config::get('fi.invoiceLogo')) }}" alt="yourlogo" class="company-logo" />
-            <p class="company-address">
-                {{ $invoice->user->company }}<br/>
-                {{ $invoice->user->address_1 }}<br/>
-                {{ $invoice->user->city }}, {{ $invoice->user->state }} {{ $invoice->user->zip }}
-            </p>
-
-            <table id="table" class="tablesorter" cellspacing="0"> 
-                <thead> 
-                    <tr> 
-                        <th>{{ trans('fi.quantity') }}</th> 
-                        <th>{{ trans('fi.name') }}</th>
-                        <th>{{ trans('fi.description') }}</th>
-                        <th>{{ trans('fi.price') }}</th> 
-                        <th>{{ trans('fi.subtotal') }}</th> 
-                    </tr> 
-                </thead> 
-                <tbody>
-                    @foreach ($invoice->items as $item)
-                    <tr>
-                        <td>{{ $item->formatted_quantity }}</td>
-                        <td>{{ $item->name }}</td>
-                        <td>{{ $item->description }}</td>
-                        <td>{{ $item->formatted_price }}</td>
-                        <td>{{ $item->amount->formatted_subtotal }}</td>
-                    </tr>
-                    @endforeach
-                </tbody> 
-            </table>
-
-            <div class="total-due">
-                <div class="total-heading"><p>{{ trans('fi.total') }}</p></div>
-                <div class="total-amount"><p>{{ $invoice->amount->formatted_total }}</p></div>
+        <header id="header">
+            <div class="invoice-intro">
+                <h1>{{ $invoice->user->company }}</h1>
             </div>
 
-            <div style="clear: both;"></div>
+            <dl class="invoice-meta">
+                <dt class="invoice-number">{{ trans('fi.invoice') }} #</dt>
+                <dd>{{ $invoice->number }}</dd>
+                <dt class="invoice-date">{{ trans('fi.invoice_date') }}</dt>
+                <dd>{{ $invoice->formatted_created_at }}</dd>
+                <dt class="invoice-due">{{ trans('fi.due_date') }}</dt>
+                <dd>{{ $invoice->formatted_due_at }}</dd>
+            </dl>
+        </header>
+        <!-- e: invoice header -->
 
-        </div>
-        <div class="page-shadow"></div>
+        <section id="parties">
 
-    </body>
+            <div class="invoice-to">
+                <h2>{{ trans('fi.invoice_to') }}:</h2>
+                <div class="vcard">
+                    {{ $invoice->client->name }}
+                    {{ $invoice->client->email }}
+
+                    <div class="adr">
+                        <div class="street-address">{{ $invoice->client->address_1 }}</div>
+                        <span class="locality">{{ $invoice->client->city }}, {{ $invoice->client->state }} {{ $invoice->client->zip }}</span>
+                        <span class="country-name">{{ $invoice->client->country }}</span>
+                    </div>
+
+                    <div class="tel">{{ $invoice->client->phone }}</div>
+                </div><!-- e: vcard -->
+            </div><!-- e invoice-to -->
+
+            <div class="invoice-from">
+                <h2>{{ trans('fi.invoice_from') }}:</h2>
+                <div class="vcard">
+                    {{ $invoice->user->name }}
+
+                    <div class="adr">
+                        <div class="street-address">{{ $invoice->user->address_1 }}</div>
+                        <span class="locality">{{ $invoice->user->city }}, {{ $invoice->user->state }} {{ $invoice->user->zip }}</span>
+                        <span class="country-name">{{ $invoice->user->country }}</span>
+                    </div>
+
+                    <div class="tel">{{ $invoice->user->phone }}</div>
+                    {{ $invoice->user->email }}
+                </div><!-- e: vcard -->
+            </div><!-- e invoice-from -->
+
+
+            <div class="invoice-status">
+                <h3>Invoice Status</h3>
+                <strong>Invoice is <em>{{ trans('fi.' . $invoice->status_text) }}</em></strong>
+            </div><!-- e: invoice-status -->
+
+        </section><!-- e: invoice partis -->
+
+        <section class="invoice-financials">
+
+            <div class="invoice-items">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>{{ trans('fi.item') }}</th>
+                            <th>{{ trans('fi.description') }}</th>
+                            <th>{{ trans('fi.quantity') }}</th>
+                            <th>{{ trans('fi.price') }}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($invoice->items as $item)
+                        <tr>
+                            <td>{{ $item->name }}</td>
+                            <td>{{ $item->description }}</td>
+                            <td>{{ $item->formatted_quantity }}</td>
+                            <td>{{ $item->formatted_price }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+
+                </table>
+            </div><!-- e: invoice items -->
+
+            <div class="invoice-totals">
+                <table>
+                    <caption>{{ trans('fi.totals') }}:</caption>
+                    <tbody>
+                        <tr>
+                            <th>{{ trans('fi.subtotal') }}:</th>
+                            <td></td>
+                            <td>{{ $invoice->amount->formatted_item_subtotal }}</td>
+                        </tr>
+
+                        @foreach ($invoice->items as $item)
+                        @if ($item->amount->tax_total <> 0)
+                        <tr>
+                            <th>{{ trans('fi.tax') }}:</th>
+                            <td>{{ $item->tax_rate->formatted_percent }} ({{ $item->tax_rate->name }})</td>
+                            <td>{{ $item->amount->formatted_tax_total }}</td>
+                        </tr>
+                        @endif
+                        @endforeach
+
+                        @foreach ($invoice->tax_rates as $invoiceTax)
+                        <tr>
+                            <th>{{ trans('fi.tax') }}:</th>
+                            <td>{{ $invoiceTax->tax_rate->formatted_percent }} ({{ $invoiceTax->tax_rate->name }})</td>
+                            <td>{{ $invoiceTax->formatted_tax_total }}</td>
+                        </tr>
+                        @endforeach
+
+                        <tr>
+                            <th>{{ trans('fi.total') }}:</th>
+                            <td></td>
+                            <td>{{ $invoice->amount->formatted_total }}</td>
+                        </tr>
+                        <tr>
+                            <th>{{ trans('fi.paid') }}</th>
+                            <td></td>
+                            <td>{{ $invoice->amount->formatted_paid }}</td>
+                        </tr>
+                        <tr>
+                            <th>{{ trans('fi.balance') }}:</th>
+                            <td></td>
+                            <td>{{ $invoice->amount->formatted_balance }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+            </div><!-- e: invoice totals -->
+
+            <div class="invoice-notes">
+                <h6>Notes &amp; Information:</h6>
+                <p>{{ $invoice->formatted_terms }}</p>
+            </div><!-- e: invoice-notes -->
+
+        </section><!-- e: invoice financials -->
+
+        <footer id="footer">
+            <p>
+                Custom invoice footer content
+            </p>
+        </footer>
+
+    </div><!-- e: invoice -->
+
+</body>
 </html>
