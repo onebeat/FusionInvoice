3c4edfa1f54f4e56d182a8a3ed4dabe97691e72b
jesseterry <jessedterry@gmail.com>
2013-12-15 19:00:07 -0700

    Docblock updates

--- app/FI/Storage/Eloquent/Repositories/ClientNoteRepository.php
+++ app/FI/Storage/Eloquent/Repositories/ClientNoteRepository.php
@@ -4,16 +4,32 @@ use FI\Storage\Eloquent\Models\ClientNote;
 
 class ClientNoteRepository implements \FI\Storage\Interfaces\ClientNoteRepositoryInterface {
 	
+	/**
+	 * Get a single record
+	 * @param  int $id
+	 * @return ClientNote
+	 */	
 	public function find($id)
 	{
 		return ClientNote::find($id);
 	}
 	
+	/**
+	 * Create a record
+	 * @param  array $input
+	 * @return int
+	 */
 	public function create($input)
 	{
-		ClientNote::create($input);
+		return ClientNote::create($input)->id;
 	}
 	
+	/**
+	 * Update a record
+	 * @param  array $input
+	 * @param  int $id
+	 * @return void
+	 */
 	public function update($input, $id)
 	{
 		$clientNote = ClientNote::find($id);
@@ -21,6 +37,11 @@ class ClientNoteRepository implements \FI\Storage\Interfaces\ClientNoteRepositor
 		$clientNote->save();
 	}
 	
+	/**
+	 * Delete a record
+	 * @param  int $id
+	 * @return void
+	 */	
 	public function delete($id)
 	{
 		ClientNote::destroy($id);
--- app/FI/Storage/Interfaces/ClientNoteRepositoryInterface.php
+++ app/FI/Storage/Interfaces/ClientNoteRepositoryInterface.php
@@ -2,12 +2,33 @@
 
 interface ClientNoteRepositoryInterface {
 	
+	/**
+	 * Get a single record
+	 * @param  int $id
+	 * @return ClientNote
+	 */	
 	public function find($id);
 	
+	/**
+	 * Create a record
+	 * @param  array $input
+	 * @return int
+	 */
 	public function create($input);
 	
+	/**
+	 * Update a record
+	 * @param  array $input
+	 * @param  int $id
+	 * @return void
+	 */
 	public function update($input, $id);
 	
+	/**
+	 * Delete a record
+	 * @param  int $id
+	 * @return void
+	 */	
 	public function delete($id);
 	
 }
