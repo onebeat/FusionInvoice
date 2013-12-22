6b02ece4aa2ffd478d027bd50f0c9c3f5d5283dc
Jesse Terry <antithesis@localhost.localdomain>
2013-12-21 23:07:04 -0700

    Start provider binds the custom field interfaces

--- app/FI/Providers/StartProvider.php
+++ app/FI/Providers/StartProvider.php
@@ -30,18 +30,22 @@ class StartProvider extends ServiceProvider {
          */
         $app->bind('FI\Storage\Interfaces\ClientNoteRepositoryInterface', 'FI\Storage\Eloquent\Repositories\ClientNoteRepository');
         $app->bind('FI\Storage\Interfaces\ClientRepositoryInterface', 'FI\Storage\Eloquent\Repositories\ClientRepository');
+        $app->bind('FI\Storage\Interfaces\ClientCustomRepositoryInterface', 'FI\Storage\Eloquent\Repositories\ClientCustomRepository');
         $app->bind('FI\Storage\Interfaces\CustomFieldRepositoryInterface', 'FI\Storage\Eloquent\Repositories\CustomFieldRepository');
         $app->bind('FI\Storage\Interfaces\EmailTemplateRepositoryInterface', 'FI\Storage\Eloquent\Repositories\EmailTemplateRepository');
         $app->bind('FI\Storage\Interfaces\InvoiceAmountRepositoryInterface', 'FI\Storage\Eloquent\Repositories\InvoiceAmountRepository');
+        $app->bind('FI\Storage\Interfaces\InvoiceCustomRepositoryInterface', 'FI\Storage\Eloquent\Repositories\InvoiceCustomRepository');
         $app->bind('FI\Storage\Interfaces\InvoiceGroupRepositoryInterface', 'FI\Storage\Eloquent\Repositories\InvoiceGroupRepository');
         $app->bind('FI\Storage\Interfaces\InvoiceItemAmountRepositoryInterface', 'FI\Storage\Eloquent\Repositories\InvoiceItemAmountRepository');
         $app->bind('FI\Storage\Interfaces\InvoiceItemRepositoryInterface', 'FI\Storage\Eloquent\Repositories\InvoiceItemRepository');
         $app->bind('FI\Storage\Interfaces\InvoiceRepositoryInterface', 'FI\Storage\Eloquent\Repositories\InvoiceRepository');
         $app->bind('FI\Storage\Interfaces\InvoiceTaxRateRepositoryInterface', 'FI\Storage\Eloquent\Repositories\InvoiceTaxRateRepository');
         $app->bind('FI\Storage\Interfaces\ItemLookupRepositoryInterface', 'FI\Storage\Eloquent\Repositories\ItemLookupRepository');
+        $app->bind('FI\Storage\Interfaces\PaymentCustomRepositoryInterface', 'FI\Storage\Eloquent\Repositories\PaymentCustomRepository');
         $app->bind('FI\Storage\Interfaces\PaymentMethodRepositoryInterface', 'FI\Storage\Eloquent\Repositories\PaymentMethodRepository');
         $app->bind('FI\Storage\Interfaces\PaymentRepositoryInterface', 'FI\Storage\Eloquent\Repositories\PaymentRepository');
         $app->bind('FI\Storage\Interfaces\QuoteAmountRepositoryInterface', 'FI\Storage\Eloquent\Repositories\QuoteAmountRepository');
+        $app->bind('FI\Storage\Interfaces\QuoteCustomRepositoryInterface', 'FI\Storage\Eloquent\Repositories\QuoteCustomRepository');
         $app->bind('FI\Storage\Interfaces\QuoteItemAmountRepositoryInterface', 'FI\Storage\Eloquent\Repositories\QuoteItemAmountRepository');
         $app->bind('FI\Storage\Interfaces\QuoteItemRepositoryInterface', 'FI\Storage\Eloquent\Repositories\QuoteItemRepository');
         $app->bind('FI\Storage\Interfaces\QuoteRepositoryInterface', 'FI\Storage\Eloquent\Repositories\QuoteRepository');
