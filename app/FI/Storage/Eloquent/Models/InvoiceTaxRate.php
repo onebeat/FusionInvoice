6742e1614b7f0678383bd932fedc28b1a946dc55
Jesse Terry <jessedterry@gmail.com>
2013-12-27 21:02:41 -0700

    Added a scope and a relationship

--- app/FI/Storage/Eloquent/Models/InvoiceTaxRate.php
+++ app/FI/Storage/Eloquent/Models/InvoiceTaxRate.php
@@ -6,14 +6,47 @@ class InvoiceTaxRate extends \Eloquent {
 
 	protected $guarded = array('id');
 
+    /*
+    |--------------------------------------------------------------------------
+    | Relationships
+    |--------------------------------------------------------------------------
+    */
+   
+   	public function invoice()
+   	{
+   		return $this->belongsTo('\FI\Storage\Eloquent\Models\Invoice');
+   	}
+
 	public function taxRate()
 	{
 		return $this->belongsTo('\FI\Storage\Eloquent\Models\TaxRate');
 	}
 
+    /*
+    |--------------------------------------------------------------------------
+    | Accessors
+    |--------------------------------------------------------------------------
+    */
+
 	public function getFormattedTaxTotalAttribute()
 	{
 		return CurrencyFormatter::format($this->attributes['tax_total']);
 	}
+
+    /*
+    |--------------------------------------------------------------------------
+    | Scopes
+    |--------------------------------------------------------------------------
+    */
+
+    public function scopeByInvoiceDateRange($query, $from, $to)
+    {
+        return $query->whereIn('invoice_id', function($query) use ($from, $to) {
+            $query->select('id')
+            ->from('invoices')
+            ->where('created_at', '>=', $from)
+            ->where('created_at', '<=', $to);
+        });
+    }
 
 }
